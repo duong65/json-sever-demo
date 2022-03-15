5ed457442aa5aaf1f36d5ce683570e027067ff34
duong65 <duong652000@gmail.com>
2022-03-15 09:55:02 +0700

    Change to user

--- generate-data.js
+++ generate-data.js
@@ -1,104 +1,86 @@
-var casual = require('casual');
-const fs = require('fs');
+var casual = require("casual");
+const fs = require("fs");
 
 // Generate random sentence
 // You don't need function call operator here
 
 // Define custom generator
-casual.define('point', function() {
-	return {
-		x: Math.random(),
-		y: Math.random()
-	};
+casual.define("point", function () {
+  return {
+    x: Math.random(),
+    y: Math.random(),
+  };
 });
-
 
 // Generate random point
 // And so on..
 
 const randomCityList = (n) => {
+  if (n <= 0) return [];
 
-    if ( n <= 0 ) return [];
+  const cityList = [];
 
-    const cityList = [];
+  // loop and push city
+  Array.from(new Array(n)).forEach(() => {
+    const city = {
+      id: casual.uuid,
+      code: casual.state_abbr,
+      name: casual.state,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
 
-    // loop and push category
-    Array.from(new Array(n)).forEach(() => {
-        const city = {
-            id: casual.state_abbr,
-            name: casual.state,
-        }
+    cityList.push(city);
+  });
 
-        cityList.push(city);
-    })
+  return cityList;
+};
 
-    return cityList;
-}
+const randomUserList = (cityList, numberOfUsers) => {
+  if (numberOfUsers <= 0) return [];
 
-const randomStudentList = (cityList, numberOfStudents) => {
+  const userList = [];
 
-    if ( numberOfStudents <= 0 ) return [];
+  for (const city of cityList) {
+    // loop and push user
+    Array.from(new Array(numberOfUsers)).forEach(() => {
+      const user = {
+        id: casual.uuid,
+        name: casual.name,
+        gender: casual.random_element(["male", "female"]),
+        age: casual.integer((from = 18), (to = 26)),
+        salary: casual.integer((from = 800), (to = 8000)),
+        description: casual.short_description,
+        cityCode: city.code,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      };
 
-    const studentList = [];
+      userList.push(user);
+    });
+  }
 
-    for (const city of cityList) {
-        // loop and push student
-        Array.from(new Array(numberOfStudents)).forEach(() => {
-            const student = {
-                id: casual.uuid,
-                name: casual.name,
-                gender: casual.random_element(['male', 'female']),
-                age: casual.integer(from = 18, to = 26),
-                mark: Math.round(casual.double(from = 1, to = 10) * 100) / 100,
-                description: casual.short_description,
-                cityCode: city.code,
-                createdAt: Date.now(),
-                updatedAt: Date.now()
-            }
-
-            studentList.push(student);
-        })
-    }
-
-    return studentList;
-}
+  return userList;
+};
 
 // IFFE
 
 (() => {
-    // random data
-    // const cityList = randomCityList(4);
-    const cityList = [
-        {
-          "code": "TX",
-          "name": "Texas"
-        },
-        {
-          "code": "KY",
-          "name": "Kentucky"
-        },
-        {
-          "code": "AL",
-          "name": "Alabama"
-        },
-        {
-          "code": "NJ",
-          "name": "New Jersey"
-        }
-      ]
-    const studentList = randomStudentList(cityList,5);
+  // random data
+  const cityList = randomCityList(6);
+  const userList = randomUserList(cityList, 8);
 
-    // prepare DB Object
-    const db = {
-        cities: cityList,
-        students: studentList,
-        profile: {
-            name: "Po",
-        },
-    };
+  // prepare DB Object
+  const db = {
+    cities: cityList,
+    users: userList,
+    profile: {
+      name: "Po",
+    },
+  };
 
-    // write db object to db.json
-    fs.writeFile('db.json', JSON.stringify(db), () => {
-        console.log("Generate successfully");
-    });
-})()
+  // write db object to db.json
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Generate successfully");
+  });
+})();
